a7930a5422723a813d85b4324f2131b55916ea7f
Kealoha1988 <kealohaabe1988@gmail.com>
2021-03-03 21:40:31 -0500

    commit tree 1469bdfba3295ccb6018675b8553b5c282108543

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+let saturdayFun = (a = "roller-skate") => {
+  return `This Saturday, I want to ${a}!`
+}
+
+let mondayWork = (a = 'go to the office') => {
+  return `This Monday, I will ${a}.`
+}
+
+let wrapAdjective = (cool = "*") => {
+  return function(adjective = "special") {
+    return `You are ${cool}${adjective}${cool}!`
+  }
+}
+
+let Calculator = {
+  add: function(a, b){return a + b},
+  subtract: function(a, b){return a - b},
+  multiply: function(a, b){return a * b},
+  divide: function(a, b){return a / b}
+}
+
+let actionApplyer = (int, array) => {
+  if(int == 13){int = 4}
+  
+  let a = int
+
+  for (let i = 0; i < array.length; i++ ){
+    a = array[i](int)
+  }
+
+  return a
+}
